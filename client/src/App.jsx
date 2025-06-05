b301cc057bc4e3a087bbefc3ec5b0d2b9c31e22a
Erick Reinoso <eds.reinoso@yavirac.edu.ec>
2025-06-05 17:14:10 -0500

    login-formulario

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { AuthProvider } from "./context/AuthContext";
 import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/registerPage";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<h1 className="text-red-500 text-4xl">Home Page</h1>}
-        />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/task" element={<h1>task Page</h1>} />
-        <Route path="/add-task" element={<h1>new Task</h1>} />
-        <Route path="/tasks/:id" element={<h1>update Task</h1>} />
-        <Route path="/profile" element={<h1>Profile</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/task" element={<h1>task Page</h1>} />
+          <Route path="/add-task" element={<h1>new Task</h1>} />
+          <Route path="/tasks/:id" element={<h1>update Task</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 export default App;
--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -1,10 +1,61 @@
-function loginPage() {
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { sigin, errors: loginErrors } = useAuth();
+  const onSubmit = handleSubmit((data) => {
+    sigin(data);
+  });
+
   return (
-    <div className="login-page">
-      <h1>Login Page</h1>
-      <p>Please enter your credentials to log in.</p>
-      {/* Add login form or components here */}
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        {loginErrors?.map((error, i) => (
+          <div
+            key={i}
+            className="bg-red-500 text-white px-4 py-2 rounded-md my-2"
+          >
+            {error}
+          </div>
+        ))}
+        <h1 className="text-2xl font-bold">Iniciar Sesión</h1>
+        <form
+          className="bg-zinc-800 max-w-md p-10 rounded-md"
+          onSubmit={onSubmit}
+        >
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Correo electrónico"
+          />
+          {errors.email && (
+            <span className="text-red-500">El correo es requerido</span>
+          )}
+          <input
+            type="password"
+            {...register("password", { required: true })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Contraseña"
+          />
+          {errors.password && (
+            <span className="text-red-500">La Contraseña es requerida</span>
+          )}
+          <button type="submit">Login</button>
+        </form>
+        <p className="flex gap-x-2 justify-between">
+          No tienes una cuenta aun{" "}
+          <Link to="/register" className="text-sky-500">
+            Registrate
+          </Link>{" "}
+        </p>
+      </div>
     </div>
   );
 }
-export default loginPage;
+export default LoginPage;
